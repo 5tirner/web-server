bd9b71361364ae4931af58101fe6809de3a1dfc8
5tirner <thatfor1337@gmail.com>
2024-01-24 17:14:59 +0100

    @5tirner

--- openFile.cpp
+++ openFile.cpp
@@ -10,11 +10,12 @@ configFile::configFile(int ac, char **av)
 {
     std::cout << "Config File Paramz" << std::endl;
     if (ac == 1)
-        this->fileName = "./default";
+        this->fileName = "default";
     else if (ac == 2)
         this->fileName = av[1];
     else
         throw BadRunException();
+    std::cout << "-> " + this->fileName << std::endl; 
     this->File.open(this->fileName.c_str(), std::ios_base::in);
     if (!this->File)
         throw BadFileException();
