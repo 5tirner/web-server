a129f84acd312bd471be9da6af4df3a465a9bce5
5tirner <thatfor1337@gmail.com>
2024-01-25 14:48:27 +0100

    @5tirner

--- pars.cpp
+++ pars.cpp
@@ -1,15 +1,15 @@
 #include "mainHeader.hpp"
 
+/*This File For Any Code About String Manupilution*/
+
 std::string removeWhiteSpaces(std::string &s)
 {
     std::string ret;
     size_t i = 0;
     if (s[i] == ' ' || s[i] == '\t')
     {
-        while ((i < s.size()) && (s[i] == ' ' || s[i] == '\t'))
-            i++;
-        if (i < s.size() && s[i] == '\n')
-            i++;
+        while ((i < s.size()) && (s[i] == ' ' || s[i] == '\t')) i++;
+        if (i < s.size() && s[i] == '\n') i++;
     }
     while (i < s.size())
     {
