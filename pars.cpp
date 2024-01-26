798d8986685a2fa5f1261ecda45d87204693aa37
5tirner <thatfor1337@gmail.com>
2024-01-26 16:44:04 +0100

    @5tirner

--- pars.cpp
+++ pars.cpp
@@ -2,6 +2,46 @@
 
 /*This File For Any Code About String Manupilution*/
 
+int isAgoodServer(std::string &server)
+{
+    int openCollad = 0;
+    int closeCollad = 0;
+    for (size_t i = 0; i < server.size(); i++)
+    {
+        if (server[i] == '{' || server[i] == '}')
+        {
+            if (server[i] == '{') openCollad++;
+            else if (server[i] == '}') closeCollad++;
+            i++;
+            while (i < server.size() && server[i] != '\n')
+            {
+                if (server[i] != ' ' && server[i] != '\t')
+                {
+                    //std::cout << '`' << server[i] << '`' << std::endl;
+                    return (1);
+                }
+                i++;
+            }
+        }
+        if (server[i] == ';')
+        {
+            i++;
+            while (i < server.size() && server[i] != '\n')
+            {
+                if (server[i] != ' ' && server[i] != '\t' && server[i] != ';')
+                {
+                    //std::cout << '`' << server[i] << '`' << std::endl;
+                    return (1);
+                }
+                i++;
+            }
+        }
+    }
+    if (closeCollad != openCollad)
+        return (1);
+    return (0);
+}
+
 int isServer(std::string &s, size_t i)
 {
     std::string check;
--- serverInfos.cpp
+++ serverInfos.cpp
@@ -1,51 +1,12 @@
 #include "mainHeader.hpp"
 
-int isAgoodServer(std::string &server)
-{
-    int openCollad = 0;
-    int closeCollad = 0;
-    for (size_t i = 0; i < server.size(); i++)
-    {
-        if (server[i] == '{' || server[i] == '}')
-        {
-            if (server[i] == '{') openCollad++;
-            else if (server[i] == '}') closeCollad++;
-            i++;
-            while (i < server.size() && server[i] != '\n')
-            {
-                if (server[i] != ' ' && server[i] != '\t')
-                {
-                    //std::cout << '`' << server[i] << '`' << std::endl;
-                    return (1);
-                }
-                i++;
-            }
-        }
-        if (server[i] == ';')
-        {
-            i++;
-            while (i < server.size() && server[i] != '\n')
-            {
-                if (server[i] != ' ' && server[i] != '\t' && server[i] != ';')
-                {
-                    //std::cout << '`' << server[i] << '`' << std::endl;
-                    return (1);
-                }
-                i++;
-            }
-        }
-    }
-    if (closeCollad != openCollad)
-        return (1);
-    return (0);
-}
-
 int servers::fillInfos(void)
 {
     for (size_t i = 0; i < this->server.size(); i++)
     {
         if (isAgoodServer(this->server[i]))
             return (1);
+
     }
     return (0);
 }
