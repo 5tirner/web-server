29488219794a5b31465da27e2d198ef19dab05e2
5tirner <thatfor1337@gmail.com>
2024-02-12 15:58:05 +0100

    @5tirner

--- multuplexing/usePoll.cpp
+++ multuplexing/usePoll.cpp
@@ -1,3 +1,4 @@
+#include <cstdlib>
 #include <poll.h>
 #include <sys/poll.h>
 #include <unistd.h>
@@ -9,13 +10,31 @@ int main()
     struct pollfd check;
     //init check;
     check.fd = 0;
-    check.events = POLLIN | POLLOUT;
+    check.events = POLLIN;
     std::cout << "Waitig For Events..." << std::endl;
+    unsigned long timer = 0;
+    char *buffer = (char *)malloc(1000);
     while (1)
     {
-        if (poll(&check, 1, 100) == 1)
+        int nfd = poll(&check, 1, 0);
+        if (nfd == 1)
         {
-            
+            std::cout << "Event Happen In Fd " << nfd << std::endl;
+            int p = read(0, buffer, 1000);
+            if (p == -1)
+            {
+                free(buffer);
+                return (1);
+            }
+            write(1, buffer, p);
+            break;
         }
-    }    
+        else
+        {
+            std::cout << "HIIII" << std::endl;
+            timer++;
+        }
+    }
+    free(buffer);
+    std::cout << "Late For " << timer << " Time" << std::endl; 
 }
