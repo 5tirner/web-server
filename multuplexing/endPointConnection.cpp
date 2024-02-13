1b5ede77283d0ff0ce16217954550ee90b52b655
5tirner <thatfor1337@gmail.com>
2024-02-13 17:35:18 +0100

    @5tirner

--- multuplexing/endPointConnection.cpp
+++ multuplexing/endPointConnection.cpp
@@ -1,6 +1,7 @@
 #include "../include/mainHeader.hpp"
 #include <asm-generic/socket.h>
 #include <cstdint>
+#include <iostream>
 #include <netinet/in.h>
 #include <sys/poll.h>
 #include <sys/socket.h>
@@ -36,7 +37,7 @@ int main()
     std::cout << "- Try Create Socket..." << std::endl;
     int serverFD = socket(AF_INET, SOCK_STREAM, 0);
     if (serverFD == -1) return (errorsGenerator("Fail To Creat Endpoint For Server", -1));
-    std::cout << "Socket Created With Number: " << serverFD << '.' << std::endl;
+    std::cout << "Socket Created Successfully With Number: " << serverFD << '.' << std::endl;
     struct sockaddr_in SI;
     SI.sin_family = AF_INET, SI.sin_port = htons(9000);
     std::cout << "- Try To Allocate Buffer Space To The Socket " << serverFD << "..." << std::endl;
@@ -54,7 +55,36 @@ int main()
     int turnPassive = listen(serverFD, INT32_MAX);
     if (turnPassive == -1)
         return (errorsGenerator("Can't Make THe Socket Passive", serverFD));
-    std::cout << "Socket Ready To LIstening." << std::endl;
-    
+    std::cout << "Socket Ready To Listening." << std::endl;
+    socklen_t grb = sizeof(SI);
+    std::cout << "Waiting For Client..." << std::endl;
+    int clientfd = accept(serverFD, (struct sockaddr*)&SI, &grb);
+    if (clientfd == -1)
+        return (errorsGenerator("Can't Create Endpoint For Client", serverFD));
+    std::cout << "Client Is Here With The Number " << clientfd << '.' << std::endl;
+    while (grb != 0)
+    {
+        char buffer[100000];
+        grb = read(clientfd, buffer, 100000);
+        if (grb == -1)
+        {
+            close(clientfd);
+            return (errorsGenerator("Can't Read From Clinet Endpoint", serverFD));
+        }
+        else if (grb)
+        {
+            std::cout << "Reading " << grb << " Bytes From Client Side, Here Are These:" << std::endl;
+            write(1, buffer, grb);
+            std::cout << std::endl;
+        }
+        if (write(clientfd, "Welcome\n", std::strlen("Welcome\n")) == -1)
+        {
+            close(clientfd);
+            return (errorsGenerator("Can't Write Into Client Endpoint", serverFD));
+        }
+        sleep(5);
+    }
+    std::cout << "Connection Closed From Client Side" << std::endl;
+    close(clientfd);
     close(serverFD);
 }
