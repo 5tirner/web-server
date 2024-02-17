ebf442a721e2ccbf3ee96ee9c310188608b9be0d
5tirner <thatfor1337@gmail.com>
2024-02-17 18:27:15 +0100

    @5tirner

--- multuplexing/poll.cpp
+++ multuplexing/poll.cpp
@@ -8,20 +8,39 @@
 #include <unistd.h>
 #include <vector>
 #include <sys/poll.h>
+#include <map>
+
+int createServerEndpoit(int *server, struct sockaddr_in *sockInfo)
+{
+    *server = socket(AF_INET, SOCK_STREAM, 0);
+    if (*server == -1) return (printf("socket Failed\n"));
+    sockInfo->sin_addr.s_addr = 0, sockInfo->sin_port = htons(9090), sockInfo->sin_family = AF_INET;
+    socklen_t a = 1;
+    int allocateBuffer = setsockopt(*server, SOL_SOCKET, SO_REUSEADDR, &a, sizeof(a));
+    if (allocateBuffer == -1) return (printf("setsockopt Failed\n"));
+    int assigningName = bind(*server, (struct sockaddr*)sockInfo, sizeof(*sockInfo));
+    if (assigningName == -1) return (printf("bind Failed\n"));
+    int passiveIt = listen(*server, INT32_MAX);
+    if (passiveIt == -1) return (printf("listen Failed\n"));
+    return (0);
+}
+struct  pollfd  fillMonitor(int fd)
+{
+    struct pollfd toFill;
+    toFill.fd = fd, toFill.events = POLLIN;
+    return (toFill);
+}
+
+void    getClientsReq(std::map<int, std::string> &clientsRequests, int fd)
+{
+}
+
 int main()
 {
-    std::string p;
-    int server = socket(AF_INET, SOCK_STREAM, 0);
-    if (server == -1) return (printf("socket Failed\n"));
-    struct sockaddr_in  sockInfo;
-    sockInfo.sin_addr.s_addr = 0, sockInfo.sin_port = htons(9090), sockInfo.sin_family = AF_INET;
-    socklen_t a = 1;
-    int allocateBuffer = setsockopt(server, SOL_SOCKET, SO_REUSEADDR, &a, sizeof(a));
-    if (allocateBuffer == -1) return (printf("setsockopt Failed\n"));
-    int assigningName = bind(server, (struct sockaddr*)&sockInfo, sizeof(sockInfo));
-    if (assigningName == -1) return (printf("bind Failed\n"));
-    int passiveIt = listen(server, INT32_MAX);
-    if (passiveIt == -1) return (printf("listen Failed\n"));
+    int    server;
+    struct sockaddr_in sockInfo;
+    if (createServerEndpoit(&server, &sockInfo) != 0)
+        return (1);
     std::cout << "waiting For Clients" << std::endl;
     std::vector<int> servFds;
     std::vector<int> clientFds;
@@ -29,29 +48,22 @@ int main()
     while (1)
     {
         struct pollfd monitor[servFds.size() + clientFds.size()];
-        size_t i = 0;
+        size_t i = 0, j = 0;
         for (; i < clientFds.size(); i++)
+            monitor[i] = fillMonitor(clientFds[i]);
+        for (; j < servFds.size(); j++)
         {
-            struct pollfd toFill;
-            toFill.fd = clientFds[i];
-            toFill.events = POLLIN | POLLOUT;
-            monitor[i] = toFill;
-        }
-        for (; i < servFds.size(); i++)
-        {
-            struct pollfd toFill;
-            toFill.fd = servFds[i];
-            toFill.events = POLLIN;
-            monitor[i] = toFill;
+            monitor[i] = fillMonitor(servFds[j]);
+            i++;
         }
         int checker = (poll(monitor, (servFds.size() + clientFds.size()), 1000));
-        if (checker == -1)
-            return (printf("poll Failed\n"));
-        else if (checker == 0) continue;
-            //std::cout << "1000ms Pass Without Any Event" << std::endl;
+        if (checker == -1)  return (printf("poll Failed\n"));
+        else if (checker == 0)  continue;
         else
         {
-            for (size_t i = 0; i < clientFds.size(); i++)
+            // watchTheFds();
+            i = 0, j = 0;
+            for (; i < clientFds.size(); i++)
             {
                 if ((monitor[i].revents & POLLIN))
                 {
@@ -72,10 +84,11 @@ int main()
                     clientFds.erase(clientFds.begin() + i);
                 }
             }
-            for (size_t i = 0; i < servFds.size(); i++)
+            for (; j < servFds.size(); j++)
             {
                 if ((monitor[i].revents & POLLIN))
                 {
+                    socklen_t a = 1;
                     int client = accept(server, (struct sockaddr*)&sockInfo, &a);
                     if (client == -1) return (printf("accept Failed\n"));
                     clientFds.push_back(client);
@@ -95,6 +108,7 @@ int main()
                     close(monitor[i].fd);
                     servFds.erase(servFds.begin() + i);
                 }
+                i++;
             }
         }
     }
